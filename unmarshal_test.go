de250ab2fab11d0ad72a809da73bf2221fc8e553
Denis Cheremisov <denis.cheremisov@ncloudtech.ru>
2018-01-22 17:21:54 +0300

    some more testing

--- unmarshal_test.go
+++ unmarshal_test.go
@@ -120,3 +120,38 @@ func TestUnmarshalArray(t *testing.T) {
 		})
 	}
 }
+
+func TestRegression(t *testing.T) {
+	data := `{ProtoInfo{github.com/shilkin/grpc-gateway/third_party/googleapis/google/api/annotations.proto{FPath=/home/emacs/Sources/go/src/github.com/shilkin/grpc-gateway/third_party/googleapis/google/api/annotations.proto},atlas/error_codes.proto{FPath=/home/emacs/Sources/schema/atlas/error_codes.proto,IPath=atlas},bazaar/common/avatar/avatar.proto{FPath=/home/emacs/Sources/schema/bazaar/common/avatar/avatar.proto},google/api/http.proto{FPath=/home/emacs/Sources/grpc-gateway/third_party/googleapis/google/api/http.proto},common/service_status.proto{FPath=/home/emacs/Sources/schema/common/service_status.proto},beef/meta.proto{FPath=/home/emacs/Sources/schema/beef/meta.proto},beef/mail.proto{FPath=/home/emacs/Sources/schema/beef/mail.proto},marker/object.proto{FPath=/home/emacs/Sources/schema/marker/object.proto},common/error.proto{FPath=/home/emacs/Sources/schema/common/error.proto},common/detail.proto{FPath=/home/emacs/Sources/schema/common/detail.proto},marker/permissions.proto{FPath=/home/emacs/Sources/schema/marker/permissions.proto},github.com/gogo/protobuf/gogoproto/gogo.proto{FPath=/home/emacs/Sources/go/src/github.com/gogo/protobuf/gogoproto/gogo.proto},atlas/atlas.proto{FPath=/home/emacs/Sources/schema/atlas/atlas.proto,IPath=atlas},common/module.proto{FPath=/home/emacs/Sources/schema/common/module.proto},marker/tag.proto{FPath=/home/emacs/Sources/schema/marker/tag.proto},common/doctype.proto{FPath=/home/emacs/Sources/schema/common/doctype.proto},google/protobuf/descriptor.proto{FPath=/home/emacs/Sources/protobuf/src/google/protobuf/descriptor.proto},bazaar/common/paging/paging.proto{FPath=/home/emacs/Sources/schema/bazaar/common/paging/paging.proto},bazaar/entities/user/user.proto{FPath=/home/emacs/Sources/schema/bazaar/entities/user/user.proto},bazaar/common/email/email.proto{FPath=/home/emacs/Sources/schema/bazaar/common/email/email.proto},common/timestamp.proto{FPath=/home/emacs/Sources/schema/common/timestamp.proto},github.com/shilkin/grpc-gateway/options/method.proto{FPath=/home/emacs/Sources/go/src/github.com/shilkin/grpc-gateway/options/method.proto},bazaar/entities/region/region.proto{FPath=/home/emacs/Sources/schema/bazaar/entities/region/region.proto}}}`
+	type (
+		// ProtoFileData содержит следующую информацию касающуюся proto-файла
+		// * абсолютный путь к proto-файлу
+		// * под каким путём сгенерированный Go-код доступен для импорта
+		ProtoFileData struct {
+			FPath string // FPath – абсолютный путь к proto-файлу
+			IPath string // IPath - путь для импорта
+		}
+
+		// PluginParams параметры плагина
+		PluginParams struct {
+			ProtoInfo map[string]ProtoFileData
+		}
+	)
+
+	v := PluginParams{}
+	if err := Unmarshal(data, &v); err != nil {
+		t.Fatal(err)
+	}
+	require.Equal(t, ProtoFileData{
+		FPath: "/home/emacs/Sources/protobuf/src/google/protobuf/descriptor.proto",
+		IPath: "",
+	}, v.ProtoInfo["google/protobuf/descriptor.proto"])
+	require.Equal(t, ProtoFileData{
+		FPath: "/home/emacs/Sources/schema/common/timestamp.proto",
+		IPath: "",
+	}, v.ProtoInfo["common/timestamp.proto"])
+	require.Equal(t, ProtoFileData{
+		FPath: "/home/emacs/Sources/schema/atlas/atlas.proto",
+		IPath: "atlas",
+	}, v.ProtoInfo["atlas/atlas.proto"])
+}
